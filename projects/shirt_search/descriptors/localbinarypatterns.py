72955efe73d6b26882fd879a0cad98d8fbc7f5a4
i008 <kubacieslik@gmail.com>
2016-03-15 23:03:01 +0100

    month3

--- projects/shirt_search/descriptors/localbinarypatterns.py
+++ projects/shirt_search/descriptors/localbinarypatterns.py
@@ -14,3 +14,14 @@ class LocalBinaryPatterns(object):
         hist /= (hist.sum() + eps)
         return hist
 
+
+
+
+
+from concurrent.futures import ProcessPoolExecutor
+
+
+L = [1,2,3,4,5]
+
+with ProcessPoolExecutor(6) as executor:
+    results = executor.map(lambda x: x**3, L)
